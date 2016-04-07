26cf55818d2e891f88fd7088c43c924a60c540cd
Solomon Greenberg <solomongreenberg@gmail.com>
2016-04-07 04:43:17 +0000

    Quick commit on 2016-04-07 01:32:54.

--- pythonCV/mjpgToConvex.py
+++ pythonCV/mjpgToConvex.py
@@ -60,13 +60,70 @@ def cvAdd(img1, img2):
 
 def findContours(img):
     """Finds contours in image, preferably binary image"""
-    img2, contours, hierarchy = \
-        cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = \
+        cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return contours, hierarchy
 
 
-x = threshHSL(srcImg, [50, 25, 34], [93, 255, 149])  # HSL thresh lower/upper
-y = threshRGB(srcImg, [110, 119, 126], [255, 255, 255])  # RGB lower/upper
-z = cvAdd(x, y)
-cv2.imshow('image', z)
-cv2.waitKey(0)
+a = threshHSL(srcImg, [50, 25, 34], [93, 255, 149])  # HSL thresh lower/upper
+b = threshRGB(srcImg, [110, 119, 126], [255, 255, 255])  # RGB lower/upper
+c = cvAdd(a, b)
+d = c
+contours, hiearchy = findContours(d)
+
+
+tmpVar = 0
+
+while len(contours) > 1:  # this inefficient mess finds the biggest contour
+    # (I think)
+    for z in range(0, len(contours)):
+        try:
+            if cv2.contourArea(contours[z]) <= tmpVar:
+                contours.pop(z)
+        except IndexError:
+            break
+        # print (str(tmpVar) + ": " + str(len(contours)) + ": " + str(z))
+    tmpVar += 1
+
+
+# rect = cv2.minAreaRect(contours[0])
+# box = cv2.cv.BoxPoints(rect)
+# box = np.int0(box)
+# cv2.drawContours(srcImg, [box], 0, (0, 255, 0), 2)
+#
+# rows, cols = srcImg.shape[:2]
+# [vx, vy, x, y] = cv2.fitLine(contours[0], cv2.cv.CV_DIST_L2, 0, 0.01, 0.01)
+# lefty = int((-x*vy/vx) + y)
+# righty = int(((cols-x)*vy/vx)+y)
+# cv2.line(srcImg, (cols-1, righty), (0, lefty), (255, 0, 0), 2)
+
+hull = cv2.convexHull(contours[0], returnPoints=True)
+
+(count,_,_) = hull.shape
+hull.ravel()
+hull.shape = (count, 2)
+
+cv2.drawContours(srcImg, contours, -1, (0, 0, 255), 3)
+# cv2.polylines(srcImg, np.int32([hull]), True, (0, 255, 0), 5)
+
+tmpVar = 0
+while len(cv2.approxPolyDP(contours[0], tmpVar, True)) != 8:
+    if len(cv2.approxPolyDP(contours[0], tmpVar, True)) > 8:
+        tmpVar += 1
+    elif len(cv2.approxPolyDP(contours[0], tmpVar, True)) < 8:
+        tmpVar -= 1
+
+approx = cv2.approxPolyDP(contours[0], tmpVar, True)
+
+print len(approx)
+
+
+cv2.drawContours(srcImg, approx, -1, (0, 255, 0), 3)
+
+cv2.imshow('e', srcImg)
+
+while True:
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
 cv2.destroyAllWindows()
